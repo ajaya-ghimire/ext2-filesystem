10019ae67cbbe23248b69e8505bf56d59ddfb6d0
Ajaya Ghimire <110246264+ajaya-ghimire@users.noreply.github.com>
2025-12-23 00:21:13 -0500

    Update usage message and integrate VDI handling

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
+#include "vdi.h"
 #include "ext2.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cout << "Usage: ./ext2 <image.img>\n";
+        std::cout << "Usage: ./ext2vdi disk.vdi\n";
         return 1;
     }
 
-    Ext2 fs(argv[1]);
+    VDI vdi(argv[1]);
+    Ext2 fs(vdi.disk, vdi.dataOffset);
     fs.printSuperblock();
-    fs.listRoot();
 
     return 0;
 }
-
