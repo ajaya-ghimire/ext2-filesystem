277c0c5c7b4b7494583a13ecf9e8c1b14683ca37
Ajaya Ghimire <110246264+ajaya-ghimire@users.noreply.github.com>
2025-12-23 00:20:59 -0500

    Refactor Ext2 constructor and superblock handling

--- ext2.cpp
+++ ext2.cpp
@@ -1,28 +1,20 @@
 #include "ext2.h"
 #include <iostream>
 
-Ext2::Ext2(const std::string& image) {
-    disk.open(image, std::ios::binary);
-    if (!disk) {
-        std::cerr << "Failed to open image\n";
-        exit(1);
-    }
+Ext2::Ext2(std::ifstream& d, uint64_t off)
+    : disk(d), offset(off) {
     readSuperblock();
 }
 
 void Ext2::readSuperblock() {
-    disk.seekg(1024); // superblock offset
-    disk.read(reinterpret_cast<char*>(&sb), sizeof(Superblock));
-    blockSize = 1024 << sb.log_block_size;
+    disk.seekg(offset + 1024);
+    disk.read(reinterpret_cast<char*>(&sb), sizeof(sb));
+    blockSize = 1024 << sb.logBlockSize;
 }
 
 void Ext2::printSuperblock() {
-    std::cout << "Inodes: " << sb.inodes_count << "\n";
-    std::cout << "Blocks: " << sb.blocks_count << "\n";
+    std::cout << "Ext2 Superblock\n";
+    std::cout << "Inodes: " << sb.inodes << "\n";
+    std::cout << "Blocks: " << sb.blocks << "\n";
     std::cout << "Block size: " << blockSize << " bytes\n";
-}
-
-void Ext2::listRoot() {
-    std::cout << "[Root directory listing simulated]\n";
-    std::cout << ".\n..\nfile1\nfile2\n";
 }
