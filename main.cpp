35f6b96392e5c51db01dec825d1ec574e2583a87
Ajaya Ghimire <110246264+ajaya-ghimire@users.noreply.github.com>
2025-12-23 00:17:01 -0500

    Add basic functionality to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
+#include "ext2.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: ./ext2 <image.img>\n";
+        return 1;
+    }
+
+    Ext2 fs(argv[1]);
+    fs.printSuperblock();
+    fs.listRoot();
+
+    return 0;
+}
 
