d1059425c9fa1cd45a1d5eb49057d72d347b915a
Ajaya Ghimire <110246264+ajaya-ghimire@users.noreply.github.com>
2025-12-23 00:20:40 -0500

    Refactor Ext2 class and Superblock structure

--- ext2.h
+++ ext2.h
@@ -2,27 +2,29 @@
 #define EXT2_H
 
 #include <cstdint>
-#include <string>
 #include <fstream>
 
 struct Superblock {
-    uint32_t inodes_count;
-    uint32_t blocks_count;
-    uint32_t log_block_size;
+    uint32_t inodes;
+    uint32_t blocks;
+    uint32_t rsvd;
+    uint32_t freeBlocks;
+    uint32_t freeInodes;
+    uint32_t firstDataBlock;
+    uint32_t logBlockSize;
 };
 
 class Ext2 {
 private:
-    std::ifstream disk;
+    std::ifstream& disk;
+    uint64_t offset;
     Superblock sb;
     uint32_t blockSize;
 
 public:
-    Ext2(const std::string& image);
+    Ext2(std::ifstream& d, uint64_t off);
     void readSuperblock();
     void printSuperblock();
-    void listRoot();
 };
 
 #endif
-
