aacda3e55b10774aaf3b20648fcbe51813cdf854
Ajaya Ghimire <110246264+ajaya-ghimire@users.noreply.github.com>
2025-12-23 00:15:30 -0500

    Add Ext2 class and Superblock structure

--- ext2.cpp
+++ ext2.cpp
@@ -1,1 +1,28 @@
+#ifndef EXT2_H
+#define EXT2_H
+
+#include <cstdint>
+#include <string>
+#include <fstream>
+
+struct Superblock {
+    uint32_t inodes_count;
+    uint32_t blocks_count;
+    uint32_t log_block_size;
+};
+
+class Ext2 {
+private:
+    std::ifstream disk;
+    Superblock sb;
+    uint32_t blockSize;
+
+public:
+    Ext2(const std::string& image);
+    void readSuperblock();
+    void printSuperblock();
+    void listRoot();
+};
+
+#endif
 
